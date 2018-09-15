94ce755dcf52b600e4a33523887dec57a81ae6d7
stardisblue <stardisblue@gmail.com>
2018-09-15 22:16:31 +0200

    adapté au format du latex

--- src/chansonTemplate.ts
+++ src/chansonTemplate.ts
@@ -7,15 +7,17 @@ moment.locale('fr')
 export default function (chanson) {
     const { title, author, url, content, date, colonnes } = chanson
 
-    const article = document.createElement('article')
-    article.style.columnCount = colonnes
-    article.classList.add('mh4', 'mv2')
-    article.innerHTML = content
+
 
     const section =  document.createElement('section')
+    section.classList.add('ma4--np')
     section.innerHTML = `<h1 class="small-caps">${title}</h1>
     <p class='f6 i'>${author ? `<span class="author">${author},</span>` : '' }
     <em class="date">dernière modification le ${moment(date).format('LLL')}</em>. <a href="${url}">permalien</a></p>`
+
+    const article = document.createElement('article')
+    article.style.columnCount = colonnes
+    article.innerHTML = content
     section.appendChild(article)
     return section
 }
